8c0e5d93dbbf35a1461241c7986f83587a4f1a4b
Shashank N <your-email@example.com>
2026-03-24 23:10:10 +0530

    remove admin auto creation (security)

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-
-python manage.py shell << END
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
-END
