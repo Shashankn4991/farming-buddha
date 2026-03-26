ed3685e3fdb63cc73aca30930ea52eef03b3475b
Shashank N <your-email@example.com>
2026-03-26 16:24:22 +0530

    remove ready() hack (production safe)

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,20 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from django.db.utils import OperationalError, ProgrammingError
-        try:
-            from django.contrib.auth import get_user_model
-            User = get_user_model()
-
-            if not User.objects.filter(username='shashankn').exists():
-                User.objects.create_superuser(
-                    username='shashankn',
-                    email='admin@example.com',
-                    password='admin123'
-                )
-                print("✅ Superuser created")
-
-        except (OperationalError, ProgrammingError):
-            pass
