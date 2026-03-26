5c3705625e3c81cb6032759858f17ee08b214bc9
Shashank N <your-email@example.com>
2026-03-26 12:10:45 +0530

    fix apps.py structure

--- bottles/apps.py
+++ bottles/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class BottlesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bottles'
@@ -11,8 +12,18 @@ class BottlesConfig(AppConfig):
             from django.contrib.auth import get_user_model
             User = get_user_model()
 
-            
+            # Reset password for shashank
+            user = User.objects.get(username='shashankn')
+            user.set_password('1234')
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+
+            print("✅ Password reset for shashank")
+
+        except User.DoesNotExist:
+            print("❌ User not found")
 
         except (OperationalError, ProgrammingError):
-            # DB not ready yet (migrations not run)
+            # DB not ready yet
             pass
