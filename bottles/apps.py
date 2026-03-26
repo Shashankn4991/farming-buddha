c1ae4b14ff2e0fa035e2ea09a06b55f1e75fec43
Shashank N <your-email@example.com>
2026-03-26 12:01:12 +0530

    remove temp superuser creation

--- bottles/apps.py
+++ bottles/apps.py
@@ -11,13 +11,7 @@ class BottlesConfig(AppConfig):
             from django.contrib.auth import get_user_model
             User = get_user_model()
 
-            if not User.objects.filter(username='admin').exists():
-                User.objects.create_superuser(
-                    username='admin',
-                    email='admin@example.com',
-                    password='admin123'
-                )
-                print("✅ Superuser created")
+            
 
         except (OperationalError, ProgrammingError):
             # DB not ready yet (migrations not run)
