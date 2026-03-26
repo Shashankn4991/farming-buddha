f69a514850ed24b66a0b845f3d55ff3cd8f2411a
Shashank N <your-email@example.com>
2026-03-26 10:30:02 +0530

    fix homepage redirect

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', lambda request: redirect('login')),
 
     path('', include('accounts.urls')),
     
